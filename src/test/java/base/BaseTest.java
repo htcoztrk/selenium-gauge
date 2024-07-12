c3b1558359ebf73bf1e5d84ba2f37021d7a9f9fc
hatice ozturk <haticeozturk0207@gmail.com>
2024-07-12 11:41:01 +0300

    Update BaseTest.java

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -149,8 +149,8 @@ public class BaseTest {
 
            // driver = new RemoteWebDriver(new URL("https://hubclouddev.testinium.com/wd/hub"), capabilities);
            // driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
-            driver = new RemoteWebDriver(new URL("http://172.25.1.25:4444/wd/hub"), capabilities);
-
+           // driver = new RemoteWebDriver(new URL("http://172.25.1.25:4444/wd/hub"), capabilities);
+            driver = new RemoteWebDriver(new URL("http://host.docker.internal:4444/wd/hub"), capabilities);
 
         }
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
